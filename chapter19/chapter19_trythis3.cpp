7ea36d6f008f7a09c3a575ae98f2dfb91c362aa2
Benjamin Wuethrich <bewuethr@users.noreply.github.com>
2014-08-31 03:24:50 -0600

    Start with third Try This in Chapter 19

---
+++ chapter19/chapter19_trythis3.cpp
@@ -0,0 +1,17 @@
+// Chapter 19, Try This 3: modify reserve to use auto_ptr, compare to
+// vector_base solution
+
+template<class T, class A>
+void vector<T,A>::reserve(int newalloc)
+{
+    if (newalloc<=space) return;        // never decrease allocation
+    T* p = alloc.allocate(newalloc);    // allocate new space
+
+    for (int i = 0; i<sz; +i) alloc.construct(&p[i],elem[i]);   // copy
+
+    for (int i = 0; i<sz; ++i) alloc.destroy(&elem[i]);         // destroy
+
+    alloc.deallocate(elem,space);       // deallocate old space
+    elem = p;
+    space = newalloc;
+}
