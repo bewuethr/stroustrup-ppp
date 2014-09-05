73e1c27ffb1d7e8ce3319f1135143faf8de471d0
Benjamin Wuethrich <bewuethr@users.noreply.github.com>
2014-09-05 16:51:35 -0600

    Update Chapter 19, exercise 10

--- chapter19/chapter19_ex10.cpp
+++ chapter19/chapter19_ex10.cpp
@@ -8,7 +8,9 @@
 
 struct Tracer {
     Tracer() { cout << "Tracer()\n"; }
+    Tracer(int n) : val(n) { cout << "Tracer(int n)\n"; }
     ~Tracer() { cout << "~Tracer()\n"; }
+    int val;
 };
 
 //------------------------------------------------------------------------------
@@ -38,16 +40,29 @@ Tracer* f()
 {
     Tracer* p1 = new Tracer();          // destructor never called
     auto_pntr<Tracer> p2(new Tracer()); // destructor called when out of scope
-    auto_pntr<Tracer> p3(new Tracer()); // released from auto_pntr
+    auto_pntr<Tracer> p3(new Tracer(5)); // released from auto_pntr
     return p3.release();
 }
 
 //------------------------------------------------------------------------------
 
+// shows how auto_pntr destroys its object when an exception is thrown
+void f2()
+{
+    Tracer* p1 = new Tracer();
+    auto_pntr<Tracer> p2(new Tracer());
+    error("Exception!");    // calls destructor of p2
+    delete p1;              // never gets called
+}
+
 int main()
 try {
     Tracer* p = f();
+    cout << "Dereference: " << (*p).val << "\n";
+    cout << "Pointer access: " << p->val << "\n";
     delete p;
+
+    f2();
 }
 catch (exception& e) {
     cerr << "exception: " << e.what() << endl;
