b2826cf3bd612b551d11425ab4d205ea03237963
Benjamin Wuethrich <bewuethr@users.noreply.github.com>
2014-12-12 12:40:02 -0500

    Add Chapter 20, exercise 17

--- chapter20/chapter20_ex15.cpp
+++ chapter20/chapter20_ex15.cpp
@@ -6,6 +6,11 @@
 // operators return a reference to the object pointed to by an element rather
 // than the pointer
 
+// Exercise 17: define an ownership_vector that holds pointers to objects like
+// pvector, but provides a mechanism for the user to decide which objects are
+// owned by the vector, i.e., which objects are deleted by the destructor. Hint:
+// Chapter 13
+
 #include "../lib_files/std_lib_facilities.h"
 
 //------------------------------------------------------------------------------
@@ -93,6 +98,45 @@ public:
 
 //------------------------------------------------------------------------------
 
+// holds pointers to objects; if the object is a pointer, it will be deleted
+// when the dtor is invoked; if it is a reference, it won't
+template<class Elem>
+class ownership_vector {
+    vector<Elem*> elems;
+    vector<Elem*> owned;
+    ownership_vector(const ownership_vector&);              // prevent copying
+    ownership_vector& operator=(const ownership_vector&);
+public:
+    typedef unsigned long size_type;
+
+    ownership_vector() : owned() { }
+    ownership_vector(Elem* d) { push_back(d); }
+    ownership_vector(Elem& d) { push_back(d); }
+
+    ~ownership_vector();
+
+    Elem* operator[](size_type n) { return elems[n]; }
+    const Elem* const operator[](size_type n) const { return elems[n]; }
+
+    void push_back(Elem* d) { elems.push_back(d); owned.push_back(d); }
+    void push_back(Elem& d) { elems.push_back(&d); }
+
+    size_type size() const { return elems.size(); }
+};
+
+//------------------------------------------------------------------------------
+
+template<class Elem>
+ownership_vector<Elem>::~ownership_vector()
+{
+    for (int i = 0; i<owned.size(); ++i) {
+        cout << "Deleting " << *owned[i] << "\n";
+        delete owned[i];
+    }
+}
+
+//------------------------------------------------------------------------------
+
 // test pvector
 void f1()
 {
@@ -125,11 +169,31 @@ void f2()
 
 //------------------------------------------------------------------------------
 
+// test ownership_vector
+void f3(int i1, int i2, int i3)
+{
+    ownership_vector<int> osv(i1);
+    // fill with pointers and references in turn
+    osv.push_back(new int(2));
+    osv.push_back(i2);
+    osv.push_back(new int(4));
+    osv.push_back(i3);
+    osv.push_back(new int(6));
+
+    for (int i = 0; i<osv.size(); ++i)
+        cout << *osv[i] << "\n";
+    // dtor called here: should destroy 2, 4 and 6
+}
+
+//------------------------------------------------------------------------------
+
 int main()
 try {
     f1();
     cout << "\n\n";
     f2();
+    cout << "\n\n";
+    f3(1,3,5);
 }
 catch (Range_error& re) {
     cerr << "bad index: " << re.index << "\n";
