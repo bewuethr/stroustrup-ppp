ef9675783b081755ec75fcf800d8aa03bee52587
Benjamin Wuethrich <bewuethr@users.noreply.github.com>
2015-01-15 01:47:54 -0500

    Add Chapter 24, Try This 1

---
+++ chapter24/chapter24_trythis1.cpp
@@ -0,0 +1,12 @@
+// Chapter 23, Try This 1: replace 333 in example with 10
+
+#include<iostream>
+#include<iomanip>
+
+int main()
+{
+    float x = 1.0/10;
+    float sum = 0;
+    for (int i = 0; i<10; ++i) sum += x;
+    std::cout << std::setprecision(15) << sum << '\n';
+}
