99612d3556563e6eb6fbcfb8fdb7bb371b3caac1
Benjamin Wuethrich <bewuethr@users.noreply.github.com>
2015-01-19 19:55:20 -0500

    Add Chapter 24, exercise 5

--- chapter24/chapter24_ex04.cpp
+++ chapter24/chapter24_ex04.cpp
@@ -1,6 +1,9 @@
 // Chapter 24, exercise 4: get Gaussian elimination example to work, test with
 // simple example
 
+// Exercise 5: try with A = { {0 1} {1 0} } and b = { 5 6 } and see it fail;
+// then try with elim_with_partial_pivot()
+
 #include<iostream>
 #include "Matrix.h"
 #include "MatrixIO.h"
@@ -55,6 +58,36 @@ void classical_elimination(Matrix& A, Vector& b)
 
 //------------------------------------------------------------------------------
 
+void elim_with_partial_pivot(Matrix& A, Vector& b)
+{
+    const Index n = A.dim1();
+
+    for (Index j = 0; j<n; ++j) {
+        Index pivot_row = j;
+
+        // look for a suitable pivot
+        for (Index k = j+1; k<n; ++k)
+            if (abs(A(k,j)) > abs(A(pivot_row,j))) pivot_row = k;
+
+        // swap the rows if we found a better pivot
+        if (pivot_row != j) {
+            A.swap_rows(j,pivot_row);
+            swap(b(j),b(pivot_row));
+        }
+
+        // elimination
+        for (Index i = j+1; i<n; ++i) {
+            const double pivot = A(j,j);
+            if (pivot==0) throw runtime_error("can't solve: pivot==0");
+            const double mult = A(i,j) / pivot;
+            A[i].slice(j) = scale_and_add(A[j].slice(j),-mult,A[i].slice(j));
+            b(i) -= mult * b(j);
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+
 Vector back_substitution(const Matrix& A, const Vector& b)
 {
     const Index n = A.dim1();
@@ -73,9 +106,17 @@ Vector back_substitution(const Matrix& A, const Vector& b)
 
 //------------------------------------------------------------------------------
 
-Vector classical_gaussian_elimination(Matrix A,Vector b)
+Vector classical_gaussian_elimination(Matrix A, Vector b)
 {
     classical_elimination(A,b);
+    return back_substitution(A,b);
+}
+
+//------------------------------------------------------------------------------
+
+Vector gaussian_elimination_w_pivot(Matrix A, Vector b)
+{
+    elim_with_partial_pivot(A,b);
     return back_substitution(A,b);
 }
 
@@ -94,6 +135,18 @@ try
 
     Vector x = classical_gaussian_elimination(A,b);
     cout << "\nx:\n" << x << '\n';
+
+    double lhs2[][2] = { { 0, 1 }, { 1, 0 } };
+    Matrix A2(lhs2);
+    double rhs2[] = { 5, 6 };
+    Vector b2(rhs2);
+
+    cout << "\nA2:\n" << A2 << '\n';
+    cout << "\nb2:\n" << b2 << '\n';
+
+    //Vector x2 = classical_gaussian_elimination(A2,b2);    // fails!
+    Vector x2 = gaussian_elimination_w_pivot(A2,b2);
+    cout << "\nx2:\n" << x2 << '\n';
 }
 catch (exception& e) {
     cerr << "Exception: " << e.what() << '\n';
