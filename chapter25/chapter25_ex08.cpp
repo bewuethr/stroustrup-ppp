4e341edfd414fb413ecc015fad33e65a31a84d27
Benjamin Wuethrich <bewuethr@users.noreply.github.com>
2015-01-31 04:18:48 -0500

    Add Chapter 25, exercise 8

---
+++ chapter25/chapter25_ex08.cpp
@@ -0,0 +1,134 @@
+// Chapter 25, exercise 8: write out the numerical values of each character on
+// your keyboard
+
+#include<iostream>
+
+using namespace std;
+
+void print(char ch)
+{
+    cout << ch << ": " << int(ch) << '\t';
+}
+
+int main()
+{
+    print('°');
+    print('+');
+    print('"');
+    print('*');
+    print('ç');
+    print('%');
+    print('&');
+    print('/');
+    print('(');
+    print(')');
+    print('=');
+    print('?');
+    print('`');
+    print('§');
+    print('1');
+    print('2');
+    cout << '\n';
+    print('3');
+    print('4');
+    print('5');
+    print('6');
+    print('7');
+    print('8');
+    print('9');
+    print('0');
+    print('\'');
+    print('^');
+    print('¦');
+    print('@');
+    print('#');
+    print('¬');
+    print('|');
+    print('¢');
+    cout << '\n';
+    print('´');
+    print('~');
+    print('Q');
+    print('W');
+    print('E');
+    print('R');
+    print('T');
+    print('Z');
+    print('U');
+    print('I');
+    print('O');
+    print('P');
+    print('è');
+    print('!');
+    print('q');
+    print('w');
+    cout << '\n';
+    print('e');
+    print('r');
+    print('t');
+    print('z');
+    print('u');
+    print('i');
+    print('o');
+    print('p');
+    print('ü');
+    print('¨');
+    print('€');
+    print('[');
+    print(']');
+    print('A');
+    print('B');
+    print('C');
+    cout << '\n';
+    print('D');
+    print('E');
+    print('F');
+    print('G');
+    print('H');
+    print('J');
+    print('K');
+    print('L');
+    print('é');
+    print('à');
+    print('£');
+    print('a');
+    print('s');
+    print('d');
+    print('f');
+    print('g');
+    cout << '\n';
+    print('h');
+    print('j');
+    print('k');
+    print('l');
+    print('ö');
+    print('ä');
+    print('$');
+    print('{');
+    print('}');
+    print('>');
+    print('Y');
+    print('X');
+    print('C');
+    print('V');
+    print('B');
+    print('N');
+    cout << '\n';
+    print('M');
+    print(';');
+    print(':');
+    print('_');
+    print('<');
+    print('y');
+    print('x');
+    print('c');
+    print('v');
+    print('b');
+    print('n');
+    print('m');
+    print(',');
+    print('.');
+    print('-');
+    print('\\');
+    cout << '\n';
+}
