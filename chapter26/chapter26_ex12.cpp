874384b8ee84ee4932881859bc28142aeba95a05
Benjamin Wuethrich <bewuethr@users.noreply.github.com>
2015-02-23 05:02:10 -0500

    Add Chapter 26, exercise 13 (same file as 12)

--- chapter26/chapter26_ex12.cpp
+++ chapter26/chapter26_ex12.cpp
@@ -1,6 +1,8 @@
 // Chapter 26, exercise 12: generate random floating point numbers and sort them
 // using std::sort(). Measure time used to sort 500,000 doubles and 5,000,000
 // doubles.
+//
+// Exercise 13: repeat with random strings of lengths in the [0:100) range
 
 #include<ctime>
 #include<cstdlib>
@@ -9,8 +11,13 @@
 #include<vector>
 #include<limits>
 #include<algorithm>
+#include<string>
 
 using namespace std;
+
+//------------------------------------------------------------------------------
+
+inline int randint(int max) { return rand()%max; }
 
 //------------------------------------------------------------------------------
 
@@ -22,29 +29,61 @@ inline double drand(double min, double max)
 
 //------------------------------------------------------------------------------
 
+string rand_string()
+{
+    int len = randint(100);
+    string s;
+    for (int i = 0; i<len; ++i)
+        s += char(randint(numeric_limits<char>::max()));
+    return s;
+}
+
+//------------------------------------------------------------------------------
+
 int main()
 try {
     srand(time(0));
-    vector<int> sizes = {500000, 5000000, 10000000};
+    vector<int> sizes = {500000, 5000000};
 
+    // use vector<double>
     for (int i = 0; i<sizes.size(); ++i) {
-        cout << "Sorting " << sizes[i] << " doubles:\n";
+        cout << "vector<double>, " << sizes[i] << " elements: ";
         double t = 0;
         for (int j = 0; j<3; ++j) {
+            clock_t t1 = clock();
+            if (t1 == clock_t(-1))
+                throw exception("sorry, no clock");
             vector<double> v(sizes[i]);
             for (int i = 0; i<v.size(); ++i)
                 v[i] = drand(numeric_limits<double>::min()/2,
                              numeric_limits<double>::max()/2);
-            clock_t t1 = clock();
-            if (t1 == clock_t(-1))
-                throw exception("sorry, no clock");
             sort(v.begin(),v.end());
             clock_t t2 = clock();
             if (t2 == clock_t(-1))
                 throw exception("sorry, clock overflow");
             t += double(t2-t1)/CLOCKS_PER_SEC;
         }
-        cout << "Time: " << t/3 << " seconds\n\n";
+        cout << t/3 << " seconds\n";
+    }
+
+    // use vector<string>
+    for (int i = 0; i<sizes.size(); ++i) {
+        cout << "vector<string>, " << sizes[i] << " elements: ";
+        double t = 0;
+        for (int j = 0; j<3; ++j) {
+            clock_t t1 = clock();
+            if (t1 == clock_t(-1))
+                throw exception("sorry, no clock");
+            vector<string> v(sizes[i]);
+            for (int i = 0; i<v.size(); ++i)
+                v[i] = rand_string();
+            sort(v.begin(),v.end());
+            clock_t t2 = clock();
+            if (t2 == clock_t(-1))
+                throw exception("sorry, clock overflow");
+            t += double(t2-t1)/CLOCKS_PER_SEC;
+        }
+        cout << t/3 << " seconds\n";
     }
 }
 catch (exception& e) {
