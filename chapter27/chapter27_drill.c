e141aba93148a0e2b5bbcbde36b37d3cf171d1f2
Benjamin Wuethrich <bewuethr@users.noreply.github.com>
2015-03-08 18:30:40 -0400

    Modify Chapter 27, drill to comply with C89 standard

--- chapter27/chapter27_drill.c
+++ chapter27/chapter27_drill.c
@@ -17,15 +17,15 @@ void my_func(char* p, int x)
 int main()
 {
     printf("Hello World!\n");
-
-    char* hello = "Hello";
-    char* world = "World!";
-    char* hello_world = (char*) malloc(strlen(hello)+strlen(world)+2);
-    strcpy(hello_world,hello);
-    hello_world[strlen(hello)] = ' ';
-    strcpy(hello_world+strlen(hello)+1,world);
-    printf("%s\n",hello_world);
-
+    {
+        char* hello = "Hello";
+        char* world = "World!";
+        char* hello_world = (char*) malloc(strlen(hello)+strlen(world)+2);
+        strcpy(hello_world,hello);
+        hello_world[strlen(hello)] = ' ';
+        strcpy(hello_world+strlen(hello)+1,world);
+        printf("%s\n",hello_world);
+    }
     my_func("foo",7);
     my_func("Scott Meyers",42);
     my_func("Bjarne Stroustrup",99);
